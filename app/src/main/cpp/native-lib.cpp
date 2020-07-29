a1fb0de6103a7b3bbbf3d46c91ca59c055681b20
semye <464622832@qq.com>
2020-07-30 01:08:38 +0800

    commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,6 +7,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_ndksample_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    string hello = "Hello from C++";
+    string hello = "Hello from C++ by yesheng";
     return env->NewStringUTF(hello.c_str());
 }
